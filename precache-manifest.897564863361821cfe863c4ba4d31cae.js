00febee3943340e8eea7b5e0cfe5b36f2967acb1
samutamm <samu@speechly.com>
2020-05-19 16:03:56 +0300

    Updates

---
+++ precache-manifest.897564863361821cfe863c4ba4d31cae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9165550a95efa3033b405d4d0c0b3309",
+    "url": "/photo-editor-demo/index.html"
+  },
+  {
+    "revision": "8fb5b69105b7e1b44459",
+    "url": "/photo-editor-demo/static/css/main.2206351d.chunk.css"
+  },
+  {
+    "revision": "6cf922c15ab552addc38",
+    "url": "/photo-editor-demo/static/js/2.91ac143f.chunk.js"
+  },
+  {
+    "revision": "565b7988fbf155b49bd527a971c54a15",
+    "url": "/photo-editor-demo/static/js/2.91ac143f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb5b69105b7e1b44459",
+    "url": "/photo-editor-demo/static/js/main.04a2eeed.chunk.js"
+  },
+  {
+    "revision": "7cca49042929348e3d42",
+    "url": "/photo-editor-demo/static/js/runtime-main.99e94e43.js"
+  },
+  {
+    "revision": "0ca5919e109effa35df5202fa45f340b",
+    "url": "/photo-editor-demo/static/media/image.0ca5919e.svg"
+  },
+  {
+    "revision": "4ecbb0d22680a2f9b14519d67a60c729",
+    "url": "/photo-editor-demo/static/media/logo.4ecbb0d2.png"
+  },
+  {
+    "revision": "5c3a2d90b6a217327abc8e181d12e556",
+    "url": "/photo-editor-demo/static/media/onboard-background.5c3a2d90.png"
+  }
+]);
